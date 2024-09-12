edfe6ba645b681cff7bdf8905f40e5661c65d926
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2024-09-12 09:21:17 +0200

    Lade till exempelkod

--- Quiz/Program.cs
+++ Quiz/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// string name = "Micke";
+
+// bool isAlive = name == "Micke";
+
+Babar();
+
+float c = 23;
+float f = 32 + (c * 1.8f);
+Console.WriteLine($"Fahrenheit: {f:00.00}");
+
+Console.WriteLine("""
+{f}
+
+""");
+
+int hp = 100;
+
+hp = hp + 10;
+hp += 10;
+hp += 1;
+hp++;
+Console.WriteLine($"Du har {hp} hp kvar");
+if (hp > 0)
+{
+  Console.WriteLine("yay! Du lever!");
+}
+Console.ReadLine();
+
+static void Babar()
+{
+Console.WriteLine("""
+          _  .   .   .
+        .' '; '-' '-'|-.
+      (     '------.'  )
+        ;            \ /
+        :     '   ' |/
+        '._._       \    .;
+        .-'   ;--.    '--' /
+      /      \-'---.___.'
+      |     / 7 \(>o<) /\
+      |     | \ |  . \   \
+      |=====|   |  .  \ |-)
+      |-'-'   ./_.-._.\|"
+      '-.----'        |
+        |       |     |
+        |     | |   | |
+        |_____|_|___|_|
+        (-------',----'.
+          '-'-----'-----'         
+    """);
+}
